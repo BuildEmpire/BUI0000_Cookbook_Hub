0b1dc6246886ab363507cbd25fb1613e5e07550d
Michael Wright <348317+michaelw90@users.noreply.github.com>
2021-01-08 10:51:49 +0000

    Update to Hub 2020.1.12722

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 default['java']['jdk_version'] = '8'
 
-default['cookbook_hub']['hub']['version'] = '2018.3.10920'
+default['cookbook_hub']['hub']['version'] = '2020.1.12722'
 default['cookbook_hub']['hub']['install_root_dir'] = '/opt/hub'
 default['cookbook_hub']['hub']['data_dir'] = '/root/.HubData'
 default['cookbook_hub']['hub']['backup_dir'] = '/root/.HubBackup'
-default['cookbook_hub']['hub']['download_url'] = 'https://download.jetbrains.com/hub/hub-2018.3.10920.zip'
+default['cookbook_hub']['hub']['download_url'] = 'https://download.jetbrains.com/hub/hub-2020.1.12722.zip'
 default['cookbook_hub']['hub']['memory_options'] = '-Xmx1g'
 
 default['cookbook_youtrack']['systemd'] = true;
